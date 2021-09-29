5e93926f7eeaf7d6db823c25208f785fb2ffd073
Samuel Schuyler <sjs08496@creighton.edu>
2021-09-29 16:22:39 -0500

    Fin

---
+++ forms/calculator/calculator.js
@@ -0,0 +1,28 @@
+function addTwo(num1,num2) {
+  let answer = num1 + num2
+  return answer
+}
+
+btnMultiply.onclick=function(){
+  num1 = inptNum1.value
+  num2 = inptNum2.value
+  
+  lblMath.value = (num1 * num2)
+}
+
+btnAdd.onclick=function(){
+ 
+  let num1 = Number(inptNum1.value)
+  let num2 = Number(inptNum2.value)
+  
+  lblMath.value = addTwo(num1,num2)
+
+}
+
+Button1.onclick=function(){
+  inptNum1.value = ""
+  inptNum2.value = ""
+  lblMath.value = ""
+}
+
+
--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,14 @@
-/*
-let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+let newMember = ""
+
+btnMember.onclick=function(){
+  newMember = inptName.value
+  if (newMember == "Mary" || newMember == "Sam" || newMember == "Joey" || newMember == "Paul" || newMember == "Mike" || newMember == "Char" || newMember == "Jenny" || newMember == "Kennedy" || newMember == "Kaden" || newMember == "Kurt" || newMember == "Anne") {
+    lblMember.value = (`${newMember}, you are already a member.`) }
+  else {
+    members[11] = newMember 
+    lblMember.value = (`${newMember}, you have been added to the list`)
+}
+}
